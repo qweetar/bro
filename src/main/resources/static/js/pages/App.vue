aae025eab9975efd442cd89ed372267081e00c54
qweetar <qweetar@gmail.com>
2019-09-07 06:54:55 +0300

    third version

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,17 +1,28 @@
 <template>
-    <div>
-    <div v-if="!profile">Authorisation with
-        <a href="/login">Google</a>
-    </div>
-    <div v-else>
-        <div>{{profile.name}}&nbsp;<a href="/logout">Log out</a></div>
-        <messages-list :messages="messages"/>
-        </div>
-    </div>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title>Bro!</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <span v-if="profile">{{profile.name}}</span>
+            <v-btn v-if="profile" icon href="/logout">
+                <v-icon>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-content>
+            <v-container v-if="!profile">
+                Authorisation with
+                <a href="/login">Google</a>
+            </v-container>
+            <v-container v-if="profile">
+                <messages-list :messages="messages"/>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
     import MessagesList from 'components/messages/MessageList.vue'
+    import { addHandler } from 'util/ws'
 
     export default {
         components: {
@@ -22,6 +33,30 @@
                 messages: frontendData.messages,
                 profile: frontendData.profile
             }
+        },
+        created() {
+           addHandler(data => {
+               if (data.objectType === 'MESSAGE') {
+                   const index = this.messages.findIndex(item => item.id === data.body.id)
+                   switch (data.eventType) {
+                       case 'CREATE':
+                       case 'UPDATE':
+                           if (index > -1) {
+                               this.messages.splice(index, 1, data.body)
+                           } else {
+                               this.messages.push(data.body)
+                           }
+                           break
+                       case 'REMOVE':
+                           this.messages.splice(index, 1)
+                           break
+                       default:
+                           console.error('Looks like the event type is unknown "${data.eventType}"')
+                   }
+               } else {
+                   console.error('Looks like the event type is unknown "${data.objectType}"')
+               }
+           })
         }
     }
 </script>
